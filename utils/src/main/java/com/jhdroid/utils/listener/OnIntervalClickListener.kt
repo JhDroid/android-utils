32e20ea416f2013ebdecc992c58bc6f43ea0c10d
JhDroid <lorzx77@gmail.com>
2021-06-02 23:47:54 +0900

    IntervalClickListener postDelayed 사용하는 방법으로 변경

--- utils/src/main/java/com/jhdroid/utils/listener/OnIntervalClickListener.kt
+++ utils/src/main/java/com/jhdroid/utils/listener/OnIntervalClickListener.kt
@@ -1,24 +1,21 @@
 package com.jhdroid.utils.listener
 
-import android.os.SystemClock
 import android.view.View
 
 class OnIntervalClickListener(
     private val listener: View.OnClickListener,
-    private val interval: Long = 2000
+    private val interval: Long = 1000
 ) : View.OnClickListener {
-    private var lastClickTime: Long = 0
+    private var isClickable = true
 
     override fun onClick(v: View) {
-        val currentClickTime = SystemClock.uptimeMillis()
-        val elapsedTime = currentClickTime - lastClickTime
-        lastClickTime = currentClickTime
+        if (isClickable) {
+            isClickable = false
+            listener.onClick(v)
 
-        // 중복 클릭으로 판단
-        if (elapsedTime <= interval) {
-            return
+            v.postDelayed({
+                isClickable = true
+            }, interval)
         }
-
-        listener.onClick(v)
     }
 }
